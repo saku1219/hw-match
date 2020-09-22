8fd33999e2965e866bbd345600dff2ff1784065a
saku1219 <saku1219@gmail.com>
2020-09-22 18:13:52 +0900

    投稿バリデーションの設定

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,29 @@
 class Work < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :type_id,     numericality: { other_than: 1, message: "is invalid" }
+    validates :name
+    validates :genre_id,    numericality: { other_than: 1, message: "is invalid" }
+    validates :place
+    validates :start_time,  if: :past_date_time
+    validates :end_time,    if: :past_start_time
+    validates :description
+    validates :user
+  end
+
+  private
+  def past_date_time
+    if start_time < Time.current
+      errors.add(:start_time, "is valid")
+    end
+  end
+
+  def past_start_time
+    if end_time < start_time || end_time < Time.current
+      errors.add(:end_time, "is valid") 
+    end
+  end
+
+
 end
