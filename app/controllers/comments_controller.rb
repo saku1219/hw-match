f5d4a801e61fa53da17fbd63ce6ff200478d44f7
saku1219 <saku1219@gmail.com>
2020-09-23 19:26:08 +0900

    コメントの保存と表示

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to  "/works/#{comment.work.id}"
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, work_id: params[:work_id])
+  end
+
+end
