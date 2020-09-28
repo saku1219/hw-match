1b02156fbc88ee72ed244515d4b388b721892d4f
saku1219 <saku1219@gmail.com>
2020-09-28 21:08:17 +0900

    messagesモデルの単体テスト

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @message = FactoryBot.build(:message)
+  end
+
+  describe 'メッセージ投稿' do
+
+    context '投稿できる' do
+      it 'メッセージが入力されていれば投稿できる' do
+        expect(@message).to be_valid
+      end
+    end
+
+    context '投稿できない' do
+      it 'メッセージが空だと投稿できない' do
+        @message.content = ""
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Content can't be blank")
+      end
+    end
+  end
 end
