5a8a6a03840fe9bbb2dd67c7fd671f244c745ed3
saku1219 <saku1219@gmail.com>
2020-09-28 18:02:05 +0900

    マイページ一覧表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
   def show
+    @user = current_user.nickname
+    @work = current_user.works.order('start_time DESC')
+    @works = current_user.works.joins(chats: :user)
+    @chat = Chat.where(work_id: @works)
   end
 end
